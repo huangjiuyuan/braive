2fb1de1aead85a35f7c0f693d0f4b05801528a71
huangjiuyuan <huangjiuyuan@163.com>
2018-03-07 11:13:53 +0000

    Revendor packages

--- vendor/golang.org/x/text/language/go1_2.go
+++ vendor/golang.org/x/text/language/go1_2.go
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build dragonfly freebsd netbsd openbsd
+// +build go1.2
 
-package unix
+package language
 
-const ImplementsGetwd = false
+import "sort"
 
-func Getwd() (string, error) { return "", ENOTSUP }
+var sortStable = sort.Stable
